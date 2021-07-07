931026223e359f3a4a8d0732cf730ead273722de
tlsrhkddus96 <tlsrhkddus96@users.noreply.github.com>
2021-07-07 17:34:03 +0900

    Re2

---
+++ src/main/java/org/zerock/re2/Re2Application.java
@@ -0,0 +1,13 @@
+package org.zerock.re2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Re2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Re2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/zerock/re2/Re2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.re2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Re2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
